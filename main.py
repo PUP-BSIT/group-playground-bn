e2b6c8e2d48be4a04e3fc1ab35264a615fd5388b
ynion-mabeamae <mabeamaeynion@gmail.com>
2024-11-24 17:12:52 +0800

    Update changes

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from pack import delima, ynion
 
 def main():
     delima.get_username()
-    ynion.calculate_circle_area()
+    
+    radius = int(input("Enter radius: "))
+    result = ynion.calculate_circle_area(radius)
+    print(f"Circle Area: {result}")
     
 main()
