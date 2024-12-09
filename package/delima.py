00051d8ade4559f482380da795db93d9f7c79210
delima_justine <delimajustine24@gmail.com>
2024-12-09 05:13:19 -0800

    add changes and comment
    * add changes to delima.py
    
    * add comment

---
+++ package/delima.py
@@ -0,0 +1,22 @@
+print("Mabuhay, I am Justine Delima")
+user_choice = 0
+
+while user_choice != 4:
+    user_choice = int(input("Enter your choice: "))
+
+    match user_choice:
+        case 1: #  Basic Info
+            print('Age: 19')
+            print('Birthdate: February 24, 2005')
+            print('Current Education: Polytechnic of the Philippines-Taguig')
+        case 2: # Goal
+            print('Goal: To have a stable life.')
+        case 3: # Comments: Hi
+            print('Comment - Name')
+            # TODO Ynion
+            # TODO Patricia Joy
+            # TODO Patricia Anne
+            # TODO Kath
+        case 4: # Exit
+            print('Goodbye.')
+    
